9806079008af4576cbb0b783518eb36314a57205
MSI\itjin <it.jing.yin@gmail.com>
2024-12-03 00:34:59 +0800

    5-7

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -317,13 +317,13 @@ GROUP BY cb.user_id;
 
 -- 5-7. 查詢：計算用戶王小明的已使用堂數，顯示須包含以下欄位： user_id , total。 (需使用到 Count 函式與 Group By)
 
--- SELECT 
--- user_id,
--- count(status) AS total
--- FROM "COURSE_BOOKING" cb 
--- WHERE user_id = ( SELECT id FROM "USER" WHERE name = '王小明' )
--- AND status = '即將授課'
--- GROUP BY user_id;
+SELECT 
+user_id,
+count(status) AS total
+FROM "COURSE_BOOKING" cb 
+WHERE user_id = ( SELECT id FROM "USER" WHERE name = '王小明' )
+AND status = '即將授課'
+GROUP BY user_id;
 
 -- 5-8. [挑戰題] 查詢：請在一次查詢中，計算用戶王小明的剩餘可用堂數，顯示須包含以下欄位： user_id , remaining_credit
     -- 提示：
